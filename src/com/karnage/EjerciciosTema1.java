42a300b4f4c29dd23c01978de42597aa30b5d088
Karnage <muaddib83@gmail.com>
2022-09-26 16:59:53 +0200

    Initial commit

---
+++ src/com/karnage/EjerciciosTema1.java
@@ -0,0 +1,22 @@
+package com.karnage;
+
+public class EjerciciosTema1 {
+
+    public static void main(String[] args) {
+
+        int numero = 100;
+
+        long numeroL = 9879987;
+
+        double numeroD = 876d;
+
+        boolean bool = true;
+
+        String nombre = "José Antonio";
+
+        System.out.println("Los tipos de datos más comunes son: " + "int: " + numero + ", long: " + numeroL
+         + ", double: " + numeroD + ", boolean: " + bool + ", y String: " + nombre);
+
+
+    }
+}
